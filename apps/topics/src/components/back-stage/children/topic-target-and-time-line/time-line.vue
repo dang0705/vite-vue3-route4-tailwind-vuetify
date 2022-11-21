9220c9d88a431749f1ad11562db652a85def1148
亨利大帝 <407278556@qq.com>
2022-11-22 00:17:53 +0800

    update

--- apps/topics/src/components/back-stage/children/topic-target-and-time-line/time-line.vue
+++ apps/topics/src/components/back-stage/children/topic-target-and-time-line/time-line.vue
@@ -25,14 +25,14 @@ export default {
 import { defineCustomElement } from 'vue';
 import { escape2Html, getSlots } from '@topics-components/utils/use-slots';
 
-let template = '<ul>';
+let template = '<ul style="margin: 0;padding: 0;">';
 
 const initTemplate = () => {
   const {
     steps: { configs }
   } = getCurrentInstance().proxy.value;
   configs.forEach(({ color: { bg, text }, content }, which) => {
-    template += `<li class='steps ' style='background-color: ${bg};color: ${text};border-color:${bg}'>${escape2Html(
+    template += `<li class='steps' style='background-color: ${bg};color: ${text};border-color:${bg};list-style: none;position:relative;display: flex'>${escape2Html(
       content
     )}${which < configs.length - 1 ? '<i></i>' : ''}</li>`;
   });
@@ -46,13 +46,21 @@ const TimeLine = defineCustomElement({
   emits: {},
   template,
   styles: [
-    'p{margin:0;padding:0}',
+    /*    'p{margin:0;padding:0}',
     'ul{margin:0;padding:0}',
     'li{list-style:none;position:relative;display:flex;justify-content:center;align-items:center;flex-direction:column}',
     'li i{position:absolute;bottom:-11px;border-top:12px;border-top-color:inherit;border-top-style:solid;border-left:10px solid transparent;border-right:10px solid transparent;width:0;height:0;}',
     '.steps{border-radius: 10px;padding:10px}',
-    '.steps +.steps{margin-top: 24px}'
+    '.steps +.steps{margin-top: 24px}'*/
   ]
 });
 customElements.define('web-steps', TimeLine);
 </script>
+<style lang="scss">
+web-steps {
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
